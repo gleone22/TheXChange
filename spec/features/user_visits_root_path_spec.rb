64b7fccd3a318ebe2e79e93b84ff3356ab32c430
George Leone <georgeleone047@gmail.com>
2016-06-01 13:29:07 -0400

    visiting root path test created

---
+++ spec/features/user_visits_root_path_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'Visiting Root' do
+  it 'displays a sign in form'
+  it 'displays the text "The Xchange"'
+  it 'displays a "Sign-Up" link'
+  it 'and clicking "Sign-Up" goes to sign_up path'
+end
